8b40bce0c65ac4d55417c63e656381ba728ac8e1
GeoCoding2004 <93879880+GeoCoding2004@users.noreply.github.com>
2025-08-14 15:16:51 +0300

    Update eslint.config.js

--- Angular/eslint.config.js
+++ Angular/eslint.config.js
@@ -1,43 +1,32 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
-
-module.exports = tseslint.config(
+module.exports = [
   {
     files: ["**/*.ts"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: ["tsconfig.json"],
+      sourceType: "module",
+    },
     extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      "plugin:@angular-eslint/recommended",
     ],
-    processor: angular.processInlineTemplates,
     rules: {
       "@angular-eslint/directive-selector": [
         "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
+        { type: "attribute", prefix: "app", style: "camelCase" },
       ],
       "@angular-eslint/component-selector": [
         "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
+        { type: "element", prefix: "app", style: "kebab-case" },
       ],
     },
   },
   {
     files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
+    extends: ["plugin:@angular-eslint/template/recommended", "plugin:@angular-eslint/template/accessibility"],
     rules: {},
-  }
-);
+  },
+];
